a6927e34b6cbb3891817bd02627e09650a244bad
Eleanor <yangjieyu@zju.edu.cn>
2019-11-27 23:18:50 +0800

    update

--- python/Linked List/148. Sort List.py
+++ python/Linked List/148. Sort List.py
@@ -41,13 +41,50 @@ class Solution:
         right = self.sortList(right)
         return mergeTwoList(left, right)
 
+    def sortList2(self, head: ListNode) -> ListNode:
+        def partition(head, end):
+            if not head or not end or head == end:
+                return
+            l1 = head
+            l2 = head.next
+            pivot = head.val
+            while l2 != end.next and l2:
+                if l2.val < pivot:
+                    l1 = l1.next
+                    if l1 != l2:
+                        tmp = l1.val
+                        l1.val = l2.val
+                        l2.val = tmp
+                l2 = l2.next
+            if head != l1:
+                tmp = l1.val
+                l1.val = head.val
+                head.val = tmp
+            return l1
+        def sortHelper(head, end):
+            if not head or not end or head == end:
+                return
+            node = partition(head, end)
+            sortHelper(head, node)
+            sortHelper(node.next, end)
+            return head
+
+        if not head or not head.next:
+            return head
+        end = head
+        while end.next:
+            end = end.next
+
+        new_head = sortHelper(head, end)
+        return new_head
+
 s = Solution()
 head = ListNode(4)
 head.next = ListNode(2)
 head.next.next = ListNode(1)
 head.next.next.next = ListNode(3)
 head.next.next.next.next = None
-new_head = s.sortList(head)
+new_head = s.sortList2(head)
 while new_head is not None:
     print(new_head.val)
     new_head = new_head.next
